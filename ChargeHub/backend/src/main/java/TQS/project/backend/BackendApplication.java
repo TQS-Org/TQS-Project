d3edebebc205bcbbe1576e439972a41b78a6dd24
João Neto <jneto04@ua.pt>
2025-05-20 14:13:57 +0100

    SCRUM-41-bdd-testing-done-still-missing-some-coverage

--- ChargeHub/backend/src/main/java/TQS/project/backend/BackendApplication.java
+++ ChargeHub/backend/src/main/java/TQS/project/backend/BackendApplication.java
@@ -1,56 +1,13 @@
 package TQS.project.backend;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import TQS.project.backend.entity.Staff;
-import TQS.project.backend.repository.ClientRepository;
-import TQS.project.backend.repository.StaffRepository;
-
-import TQS.project.backend.entity.Client;
-import TQS.project.backend.entity.Role;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
-	}
-
-	@Bean
-	CommandLineRunner init(ClientRepository clientRepo, StaffRepository staffRepo, PasswordEncoder encoder) {
-		return args -> {
-			if (clientRepo.findByMail("driver@mail.com").isEmpty()) {
-				Client client = new Client("Driver One", encoder.encode("driverpass"), 30, "driver@mail.com",
-						"123456789");
-				clientRepo.save(client);
-			}
-
-			if (staffRepo.findByMail("operator@mail.com").isEmpty()) {
-				Staff operator = new Staff();
-				operator.setName("Operator One");
-				operator.setMail("operator@mail.com");
-				operator.setPassword(encoder.encode("operatorpass"));
-				operator.setAge(35);
-				operator.setRole(Role.OPERATOR);
-				operator.setActive(true);
-				staffRepo.save(operator);
-			}
-
-			if (staffRepo.findByMail("admin@mail.com").isEmpty()) {
-				Staff admin = new Staff();
-				admin.setName("Admin One");
-				admin.setMail("admin@mail.com");
-				admin.setPassword(encoder.encode("adminpass"));
-				admin.setAge(40);
-				admin.setRole(Role.ADMIN);
-				admin.setActive(true);
-				staffRepo.save(admin);
-			}
-		};
 	}
 
 }
