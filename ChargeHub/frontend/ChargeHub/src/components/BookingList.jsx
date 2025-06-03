440d915cc49be777ffadf8c0824ec7d00856d348
Rodrigo Abreu <rgana22svb@gmail.com>
2025-06-03 22:05:11 +0100

    pushed, but has bugs

--- ChargeHub/frontend/ChargeHub/src/components/BookingList.jsx
+++ ChargeHub/frontend/ChargeHub/src/components/BookingList.jsx
@@ -1,4 +1,3 @@
-// components/BookingList.jsx
 import { useEffect, useState } from "react";
 import BookingCard from "./BookingCard";
 import CONFIG from "../../config";
@@ -7,29 +6,43 @@ export default function BookingList({ chargerId, selectedDate }) {
   const [bookings, setBookings] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  console.error("Charger ID:", chargerId);
+
   useEffect(() => {
     const fetchBookings = async () => {
       const token = localStorage.getItem("token");
-      const formattedDate = selectedDate.toLocaleDateString("en-CA");
+      if (!token) {
+        console.error("Missing auth token.");
+        setLoading(false);
+        return;
+      }
+
+      const formattedDate = selectedDate.toISOString().split("T")[0]; // 'YYYY-MM-DD'
+
+      console.log("date: ", selectedDate)
+      console.log("formated date: ", formattedDate)
 
       try {
         const res = await fetch(
           `${CONFIG.API_URL}booking/charger/${chargerId}?date=${formattedDate}`,
           {
             headers: {
+              "Content-Type": "application/json",
               Authorization: `Bearer ${token}`,
             },
+            credentials: "include",
           }
         );
+        console.log("TOKEN IN FETCH:", token);
 
         if (res.ok) {
           const data = await res.json();
           setBookings(data);
         } else {
-          console.error("Failed to fetch bookings.");
+          console.error("Failed to fetch bookings. Status:", res.status);
         }
       } catch (err) {
-        console.error(err);
+        console.error("Fetch error:", err);
       } finally {
         setLoading(false);
       }
